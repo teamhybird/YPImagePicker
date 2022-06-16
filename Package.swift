53967361695092460ff2431b4d2b3fa3dffe2155
Carl Muller <carl@hybirdtech.com>
2022-06-16 20:41:02 +0100

    Updated ios target build so it works on mac

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "YPImagePicker",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v14)
     ],
     products: [
         .library(name: "YPImagePicker", targets: ["YPImagePicker"])
